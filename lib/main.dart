6da9b6cdcba65627e83e5b19529e7812fdc781a9
mobaid75 <Mohamad.m.alobaid75@gmail.com>
2025-11-21 14:53:47 +0000

    Add logo

--- lib/main.dart
+++ lib/main.dart
@@ -112,6 +112,10 @@ class _OrderScreenState extends State<OrderScreen> {
 
     return Scaffold(
       appBar: AppBar(
+        leading: SizedBox(
+          height: 100,
+          child: Image.asset('assets/images/logo.png'),
+        ),
         title: const Text(
           'Sandwich Counter',
           style: heading1,
