10c39d8de5d36b6287f83aee532007216defe5cb
mobaid75 <Mohamad.m.alobaid75@gmail.com>
2025-12-11 14:29:39 +0000

    update the files to fix problems

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -9,13 +9,13 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppScaffold(
       title: 'About Us',
-      child: const Padding(
-        padding: EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Welcome to Sandwich Shop!', style: heading2),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text(
               'We are a family-owned business dedicated to serving the best sandwiches in town. ',
               style: normalText,
